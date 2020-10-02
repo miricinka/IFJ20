e30f607cdd0d202bca7013bf973bdb0942b46d62
Martin Žovinec <49172488+xzovin00@users.noreply.github.com>
2020-10-02 23:50:56 +0200

    Update numToName.sh

--- Scripts/numToName.sh
+++ Scripts/numToName.sh
@@ -5,6 +5,7 @@ do
     # if $command is a number then it goes into grep else it is printed
     if [ $skip=false ] && [ "$command" -eq "$command" ]  2>/dev/null; then
         grep -oh "\w*\s*\b$command\b" scanner.h 
+        # if the command is something that can be followed by a string, then its followup is immidiatly printed so it doesnt go to grep
         if [ $command -eq 0 ] || [ $command -eq 30 ] || [ $command -eq 31 ] || [ $command -eq 32 ]; then
             read command
             echo "$command"
