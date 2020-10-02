bee4778f15250a71caa8bb2e8386f6c9cef1c840
Martin Žovinec <49172488+xzovin00@users.noreply.github.com>
2020-10-02 23:49:35 +0200

    Updated script

--- Scripts/numToName.sh
+++ Scripts/numToName.sh
@@ -2,6 +2,15 @@
 
 while read command
 do
-    grep -oh "\w*\s*[^(IFJ)]$command" scanner.h 
+    # if $command is a number then it goes into grep else it is printed
+    if [ $skip=false ] && [ "$command" -eq "$command" ]  2>/dev/null; then
+        grep -oh "\w*\s*\b$command\b" scanner.h 
+        if [ $command -eq 0 ] || [ $command -eq 30 ] || [ $command -eq 31 ] || [ $command -eq 32 ]; then
+            read command
+            echo "$command"
+        fi
+    else
+        echo "$command"
+    fi
 done
 
