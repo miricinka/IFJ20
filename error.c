0dee07c5bb0537d625f8c27f977383e0affe5a32
xrezac21 <70648972+xrezac21@users.noreply.github.com>
2020-10-22 18:42:50 +0200

    Add files via upload

---
+++ error.c
@@ -0,0 +1,23 @@
+/***************************
+ * PROJECT: 
+ *  IFJ20 - Compiler for imperative programming language IFJ20
+ * 
+ * UNIVERSITY: 
+ *  Faculty of Information Technology, Brno University of Technology
+ * 
+ * FILE:
+ *  error.c
+ * 
+ * DESCRIPTION:
+ *  Error description
+ * 
+ * AUTHORS:
+ *  Rezac Ales    <xrezac21@stud.fit.vutbr.cz>
+*/
+
+#include "error.h"
+
+void errorMsg(int error_number, char* errorMessage){
+    fprintf(stderr,"Error number: %i) -- %s\n", error_number, errorMessage);
+    exit(error_number);
+}
